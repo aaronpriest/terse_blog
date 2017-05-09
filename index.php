6ea9a2101f9ef5c0126190e40a64fb65ea97d61c
Aaron Priest <apriest@saturnsys.com>
2017-05-09 15:57:41 -0500

    Added some initial files

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<title>Terse. The first poetry blog to blog in poems.</title>
+
+ <!--Styles, scripts, other functional header content-->
+ 	<link rel="stylesheet" type="text/css" href="styles.css?rf=<?php echo rand();?>" / >
+ <!--Styles and scripts-->
+
+  </head>
+
+  <body>
+    <div id="ContainerForAllContent">
+    	<h1 id="WebsiteName">Terse.</h1>
+    	<span>The first poetry blog to blog in poems.</span>
+    	<div id="ThePoemContainer">
+    		<h2 id="ThePoemsTitle">City<span id="DateWritten"> - May 4, 2017</span></h2>
+    			<span id="PoemBody">
+          <p>Today, the city looks like a yard sale</p>
+          <p>Spread carelessly across the sleeping giant's side</p>
+  				<p>Without color, without leaves,</p>
+  				<p>There is nothing to knit it together.</p>
+  				<p>It makes me weary to look at it.</p>
+  				<p>There are times. Oh, there are times</p>
+  				<p>When I'd like to pick up the edge of it</p>
+  				<p>Like my mother would do with sheets from the line</p>
+  				<p>And with a great billowing lift and a snap</p>
+  				<p>That softly clubs the air</p>
+  				<p>Launch all the streets and buildings</p>
+  				<p>The twisted metal the twisted metallic dreams</p>
+  				<p>Into forgetful Superior</p>
+  				<p>And enjoy the quiet of something that has been removed</p>
+  				<p>For the birds to sing me back to life</p><p>For the time it takes</p>
+  				<p>&#169; 2012-2017 Aaron B. Priest</p>
+  			</span><!--PoemBody-->
+      </div><!--ThePoem-->
+  	</body>
+</html>
