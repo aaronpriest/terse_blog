0c6ed10410d93f9e90ee4cf1773d5a3c9fe1e0cd
Aaron Priest <apriest@saturnsys.com>
2017-05-10 15:21:04 -0500

    Added some helpful files; a favicon; a new background; a set of CSS files to choose from

---
+++ devtools.js
@@ -0,0 +1,19 @@
+/* Dev Tools
+Functions for help with development
+*/
+function markAllBorders(){
+    var colors = ["Crimson","DarkBlue","DarkGreen","BlueViolet","DeepPink","Green","MediumSlateBlue","Tomato","Navy","OliveDrab","Purple","SeaGreen","MediumVioletRed"];
+var divs = document.getElementsByTagName("div");
+var j = 0; //see note within the for
+    for(i=0; i<divs.length; i++){
+
+        if(j>colors.length-1) j=i-j;
+        //we want i and j to be the same as long as there
+        //are not more divs than colors, but when colors run
+        //out we want to set the index for colors back to 0
+        //and continue through the array.
+
+        divs[i].style.border="1px  dotted "+colors[j];
+        j++;
+    }
+}
--- index.php
+++ index.php
@@ -2,37 +2,46 @@
 <html>
   <head>
   	<title>Terse. The first poetry blog to blog in poems.</title>
+ <!--All Meta-content-->
+    <meta name=viewport content='width=500'>
 
  <!--Styles, scripts, other functional header content-->
- 	<link rel="stylesheet" type="text/css" href="styles.css?rf=<?php echo rand();?>" / >
- <!--Styles and scripts-->
+ 	<link rel="stylesheet" type="text/css" href="styles-original.css?rf=<?php echo rand();?>" / >
+    <script src="devtools.js" type="text/javascript"></script>
+    <link rel="icon" href="favicon.ico" />
+<!--Styles and scripts-->
 
   </head>
 
   <body>
+    <div id="BannerWithMenus"><a class="menu_item">Menu</a></div>
     <div id="ContainerForAllContent">
-    	<h1 id="WebsiteName">Terse.</h1>
-    	<span>The first poetry blog to blog in poems.</span>
+    	<h1 class="block2inline" id="WebsiteName">Terse.</h1>
+        <span>The first poetry blog to blog in poems.</span>
     	<div id="ThePoemContainer">
-    		<h2 id="ThePoemsTitle">City<span id="DateWritten"> - May 4, 2017</span></h2>
+    		<h2 id="ThePoemsTitle">City<span class="dates_and_other_small_text"> - May 4, 2017</span></h2>
     			<span id="PoemBody">
-          <p>Today, the city looks like a yard sale</p>
-          <p>Spread carelessly across the sleeping giant's side</p>
+                <p>Today, the city looks like a yard sale</p>
+                <p>Spread carelessly across the sleeping giant's side</p>
   				<p>Without color, without leaves,</p>
   				<p>There is nothing to knit it together.</p>
   				<p>It makes me weary to look at it.</p>
-  				<p>There are times. Oh, there are times</p>
-  				<p>When I'd like to pick up the edge of it</p>
+  				<p>There are times--oh, there are times</p>
+  				<p>When I'd like to pick it up by it's edge</p>
   				<p>Like my mother would do with sheets from the line</p>
   				<p>And with a great billowing lift and a snap</p>
-  				<p>That softly clubs the air</p>
+  				<p>Softly thudding on the air</p>
   				<p>Launch all the streets and buildings</p>
-  				<p>The twisted metal the twisted metallic dreams</p>
+  				<p>The twisted metal and the twisted metallic dreams</p>
   				<p>Into forgetful Superior</p>
   				<p>And enjoy the quiet of something that has been removed</p>
-  				<p>For the birds to sing me back to life</p><p>For the time it takes</p>
-  				<p>&#169; 2012-2017 Aaron B. Priest</p>
+  				<p>For the time it takes the birds to sing me back to life</p>
+  				<p class="block2inline">&#169; 2012-2017 Aaron B. Priest</p>
   			</span><!--PoemBody-->
       </div><!--ThePoem-->
+
+  </div><!--ContainerForAllContent-->
+  <div id="bottom"></div>
+     <input type="button" name="ShowBorders" value="See Borders" onclick="markAllBorders()"/>
   	</body>
 </html>
