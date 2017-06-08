1fb791062f2831894ebb33595d585f0f31bc2d7a
Aaron Priest <apriest@saturnsys.com>
2017-06-08 15:11:41 -0500

    dev_tools.js added

--- Scripts/devtools.js
+++ Scripts/devtools.js
@@ -4,18 +4,22 @@ Functions for help with development
 function markAllBorders(){
 
     var colors = ["Crimson","DarkBlue","DarkGreen","BlueViolet","DeepPink","Green","MediumSlateBlue","Tomato","Navy","OliveDrab","Purple","SeaGreen","MediumVioletRed"];
-var divs = document.getElementsByTagName("article");
-var j = 0; //see note within the for
-    for(i=0; i<divs.length; i++){
+    var elements=["body","header","p","h1","h2","h3","nav","main","time","section","aside ","a"];
 
-        if(j>colors.length-1) j=i-j;
-        //we want i and j to be the same as long as there
-        //are not more divs than colors, but when colors run
-        //out we want to set the index for colors back to 0
-        //and continue through the array.
+    for(e=0; e<elements.length; e++){
+      var thisTag = document.getElementsByTagName(elements[e]);
+      var j = 0; //see note within the for
+          for(i=0; i<thisTag.length; i++){
 
-        divs[i].style.border="1px  dotted "+colors[j];
-        j++;
-    }
+              if(j>colors.length-1) j=i-j;
+              //we want i and j to be the same as long as there
+              //are not more divs than colors, but when colors run
+              //out we want to set the index for colors back to 0
+              //and continue through the array.
+
+              thisTag[i].style.border="1px  dotted "+colors[j];
+              j++;
+          }
+        }
 }
 //<input type="button" name="ShowBorders" value="See Borders" onclick="markAllBorders()"/>
